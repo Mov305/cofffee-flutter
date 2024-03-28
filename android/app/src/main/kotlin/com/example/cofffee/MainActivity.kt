44cc731add1ed79b420fd21707871a36566d7b4c
Abdelrhman Samy Saad <abdelrhman.samy.saad@gmail.com>
2024-03-28 11:02:22 +0200

    Implement new feature XYZ

---
+++ android/app/src/main/kotlin/com/example/cofffee/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cofffee
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Text("test", style: TextStyle(fontSize: 20)),
+  ));
+}
